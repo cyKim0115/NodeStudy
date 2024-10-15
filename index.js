00970c526f9d6f044870a8a4df90a7a135a1380d
cyKim <76chanyoung@gmail.com>
2024-10-16 00:18:48 +0900

    ejs를 이용한 Response

--- index.js
+++ index.js
@@ -2,39 +2,31 @@ var express = require('express')
 var app = express()
 var bodyparser = require('body-parser')
 
-app.listen(3000, function (){
+app.listen(3000, function () {
     console.log('start, asdasd')
 })
 
 app.use(express.static('public'))
 app.use(bodyparser.json())
-app.use(bodyparser.urlencoded({extended:true}))
+app.use(bodyparser.urlencoded({extended: true}))
+app.set('view engine', 'ejs')
 
-app.get('/',function (req,res){
+app.get('/', function (req, res) {
     console.log('test123')
     res.sendFile(__dirname + '/public/main.html')
 })
 
-app.get('/js',function (req,res){
-    console.log('js dir is ' + __dirname + '/public/main.js')
-    res.sendFile(__dirname + '/public/main.js')
+app.get('/email_post', function (req, res) {
+    res.sendFile(__dirname + '/public/form.html')
 })
 
-app.get('/email_post',function (req,res){
-    res.sendFile(__dirname+'/public/form.html')
-})
-
-app.post('/email_post',function (req,res){
+app.post('/email_post', function (req, res) {
     console.log(req.body)
 
-    if(req.body.email === '')
-    {
+    if (req.body.email === '') {
         res.send('No Email ?');
+    } else {
+        // res.send('Welcom ' + req.body.email)
+        res.render('email.ejs',{'email':req.body.email})
     }
-    else
-    {
-        res.send('Welcom ' + req.body.email)
-    }
-
-
 })
