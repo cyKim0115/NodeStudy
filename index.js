e910ea711aba6b544bf360c229f6b937c4d46dc3
cyKim <76chanyoung@gmail.com>
2024-10-19 11:58:54 +0900

    간단한 예제, 검색창 만들어보기

--- index.js
+++ index.js
@@ -38,3 +38,25 @@ app.post('/ajax_send_email',function (req,res){
     var responseData = {'result':'ok','email':req.body.email}
     res.json(responseData);
 })
+
+app.get('/search',function (req,res){
+    res.sendFile(__dirname + '/public/search.html')
+})
+
+app.post('/search_post',function(req,res){
+    console.log(req.body.search_string)
+
+    var responseData = {'result':'ok','search_string':req.body.search_string,'search_result':''}
+
+    switch(req.body.search_string)
+    {
+        case '강아지': responseData.search_result = '멍멍'
+            break
+        case '고양이': responseData.search_result = '야옹'
+            break
+        default: responseData.search_result = '검색 결과가 없습니다.'
+            break
+    }
+
+    res.json(responseData)
+})
