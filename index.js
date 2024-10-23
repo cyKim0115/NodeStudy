755143f53c9d483e5b6d0a1e2bace846f7e07ca0
cyKim <76chanyoung@gmail.com>
2024-10-23 23:32:02 +0900

    라우팅 모듈화, DB를 포함한 세팅들 분리

--- index.js
+++ index.js
@@ -3,16 +3,7 @@ var app = express()
 var bodyparser = require('body-parser')
 var mysql = require('mysql2')
 var main = require('./router/main')
-
-var connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: 'Megum1ng!',
-    database: 'jsman'
-})
-
-connection.connect();
+var email = require('./router/email')
 
 app.listen(3000, function () {
     console.log('server is start')
@@ -24,49 +15,11 @@ app.use(bodyparser.urlencoded({extended: true}))
 app.set('view engine', 'ejs')
 
 app.use('/main', main)
+app.use('/email',email)
 
 app.get('/', function (req, res) {
     console.log('test123')
     res.sendFile(__dirname + '/public/main.html')
-})
-
-app.get('/email_post', function (req, res) {
-    res.sendFile(__dirname + '/public/form.html')
-})
-
-app.post('/email_post', function (req, res) {
-    console.log(req.body)
-
-    if (req.body.email === '') {
-        res.send('No Email ?');
-    } else {
-        // res.send('Welcom ' + req.body.email)
-        res.render('email.ejs', {'email': req.body.email})
-    }
-})
-
-app.post('/ajax_send_email', function (req, res) {
-    console.log(req.body.email)
-
-    var email = req.body.email
-    var responseData = {}
-
-    var query = connection.query(`select name
-                                  from user
-                                  where email = "${email}"`, function (err, rows) {
-        if (err) throw err
-
-        if (rows[0]) {
-            console.log(rows[0].name)
-            responseData.result = "ok"
-            responseData.name = rows[0].name
-        } else {
-            responseData.result = "none"
-            responseData.name = ""
-        }
-
-        res.json(responseData)
-    })
 })
 
 app.get('/search', function (req, res) {
---
+++ router/email.js
@@ -0,0 +1,58 @@
+var express = require('express')
+var app = express()
+var router = express.Router()
+var path = require('path')
+const mysql = require("mysql2");
+
+// Database Setting
+var connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'Megum1ng!',
+    database: 'jsman'
+})
+
+connection.connect();
+
+// Router
+router.get('/form', function (req, res) {
+    res.sendFile(path.join(__dirname, '../public/form.html'))
+})
+
+router.post('/form', function (req, res) {
+    console.log(req.body)
+
+    if (req.body.email === '') {
+        res.send('No Email ?');
+    } else {
+        // res.send('Welcom ' + req.body.email)
+        res.render('email.ejs', {'email': req.body.email})
+    }
+})
+
+router.post('/ajax', function (req, res) {
+    console.log(req.body.email)
+
+    var email = req.body.email
+    var responseData = {}
+
+    var query = connection.query(`select name
+                                  from user
+                                  where email = "${email}"`, function (err, rows) {
+        if (err) throw err
+
+        if (rows[0]) {
+            console.log(rows[0].name)
+            responseData.result = "ok"
+            responseData.name = rows[0].name
+        } else {
+            responseData.result = "none"
+            responseData.name = ""
+        }
+
+        res.json(responseData)
+    })
+})
+
+module.exports = router
