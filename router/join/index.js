45972fea46cd667742000ab26771b7083bbb9c5c
cyKim <76chanyoung@gmail.com>
2024-11-06 23:29:10 +0900

    lcoal-strategy 콜백 생성중

--- router/join/index.js
+++ router/join/index.js
@@ -3,7 +3,7 @@ var app = express()
 var router = express.Router()
 var path = require('path')
 const mysql = require("mysql2");
-var passport= require('passport')
+var passport = require('passport')
 var LocalStrategy = require('passport-local').Strategy;
 
 // Database Setting
@@ -19,24 +19,42 @@ connection.connect();
 
 // Router
 router.get('/', function (req, res) {
-    console.log('get join url')
-    // res.sendFile(path.join(__dirname, '../../public/join.html'))
-    res.render('join.ejs');
+    var msg;
+    var errMsg = req.flash('error')
+    if (errMsg) msg = errMsg;
+    res.render('join.ejs', {"message": msg});
 })
+
+// passport.serializeUser()
 
 passport.use('local-join', new LocalStrategy({
-    usernameField: 'email',
-    passwordField: 'password',
-    passReqToCallback: true,
-}, function (req, email, password, done) {
-    console.log('local-join callback called')
-})
+        usernameField: 'email',
+        passwordField: 'password',
+        passReqToCallback: true,
+    }, function (req, email, password, done) {
+        var query = connection.query('select * from user where email=?', [email], function (err, rows) {
+                if (err) return done(err);
+
+                if (rows.length) {
+                    console.log('existed user')
+                    return done(null, false, {message: 'your email is already used'})
+                } else {
+                    var sql = {email: email, pw: password};
+                    var query = connection.query('insert into user set ?', sql, function (err, rows) {
+                        if (err) throw err
+                        return done(null, {'email': email, 'id': rows.insertId})
+                    })
+                }
+            }
+        )
+    })
 )
 
-router.post('/',passport.authenticate('local-join', {
-    successRedirect:'/main',
-    failureRedirect:'/join',
-    failureFlash:true})
+router.post('/', passport.authenticate('local-join', {
+        successRedirect: '/main',
+        failureRedirect: '/join',
+        failureFlash: true
+    })
 )
 
 /*
